3039f1846d7a0ad8701bfb89077be3ef7dc12834
Bogyeong Kim <boggyhint@gmail.com>
2022-10-06 23:30:54 +0900

    create Annotations

---
+++ src/main/java/httpclient/annotation/parameter/Body.java
@@ -0,0 +1,12 @@
+package httpclient.annotation.parameter;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Body {
+    Class<?> value();
+}
